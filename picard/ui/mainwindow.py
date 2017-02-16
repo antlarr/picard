f6a988550ea2ded925ab1efef649cc0355c1cb77
Antonio Larrosa <larrosa@kde.org>
2017-02-16 23:50:09 +0100

    Update album images when an album is to be shown
    When a user drops different images to be used as covers of
    different tracks of the same album, the album's metadata.images list
    didn't reflect these changes. This commit updates the list of images
    so CoverArtBox always shows the correct values

--- picard/ui/mainwindow.py
+++ picard/ui/mainwindow.py
@@ -934,6 +934,7 @@ class MainWindow(QtGui.QMainWindow):
                     self.set_statusbar_message(msg, mparms, echo=None,
                                                history=None)
             elif isinstance(obj, Album):
+                obj.update_metadata_images()
                 metadata = obj.metadata
                 orig_metadata = obj.orig_metadata
             elif obj.can_edit_tags():
